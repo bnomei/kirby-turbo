6b583530423770a3208bc50cd9b1eaf318f7ebf8
Bruno Meilick <b@bnomei.com>
2024-12-25 21:05:04 +0000

    :construction: wip

--- tests/site/config/config.php
+++ tests/site/config/config.php
@@ -8,6 +8,7 @@ return [
         'locking' => false,
     ],
 
+    'bnomei.turbo.license' => '',
     // 'bnomei.turbo.cache.cmd' => ['type' => 'preload-redis', 'database' => 6], // preload this data with turbo-redis
     // 'bnomei.turbo.cache.storage' => ['type' => 'redis', 'database' => 5], // load this data on demand
     // 'bnomei.turbo.cache.tub' => ['type' => 'turbo-redis', 'database' => 5], // load this data on demand
