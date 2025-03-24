2b138e4c23d937876c7744f02932095269a53e5f
Bruno Meilick <b@bnomei.com>
2025-03-24 20:38:34 +0000

    :construction: wip

--- classes/TurboPage.php
+++ classes/TurboPage.php
@@ -1,4 +1,11 @@
 <?php
+/**
+ * Copyright (c) 2025 Bruno Meilick
+ * All rights reserved.
+ *
+ * This file is part of Kirby Turbo and is proprietary software.
+ * Unauthorized copying, modification, or distribution is prohibited.
+ */
 
 declare(strict_types=1);
 
